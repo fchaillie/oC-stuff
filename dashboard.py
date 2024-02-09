539c214d2b2c0c612b11e2551402a1ac088e3678
Florent Chaillie <florentchaillie@gmail.com>
2024-02-09 21:26:14 +0100

    changed the names of the labels of the text boxes

--- dashboard.py
+++ dashboard.py
@@ -6,46 +6,55 @@ import mlflow
 import json
 import pickle
 import gzip
-
+import base64
 
 def main():
     
-    # comment
-    # st.title('')
+
     st.markdown("<h1 style='text-align: center; black: red;'>Loan Details</h1>", unsafe_allow_html=True)
+
     st.text("")
     
-    EXT_SOURCE_1 = st.number_input('EXT_SOURCE_1', min_value = 0., max_value = 1.)
+    EXT_SOURCE_1 = st.number_input('External data source 1 score', min_value = 0., max_value = 1.)
 
-    EXT_SOURCE_2 = st.number_input('EXT_SOURCE_2', min_value = 0., max_value = 1.)
+    EXT_SOURCE_2 = st.number_input('External data source 2 score', min_value = 0., max_value = 1.)
 
-    EXT_SOURCE_3 = st.number_input('EXT_SOURCE_3', min_value = 0., max_value = 1.)
+    EXT_SOURCE_3 = st.number_input('External data source 3 score', min_value = 0., max_value = 1.)
 
-    PAYMENT_RATE = st.number_input('PAYMENT_RATE', min_value = 0., max_value = 1.)
+    PAYMENT_RATE = st.number_input('Payment rate', min_value = 0., max_value = 1.)
 
-    DAYS_BIRTH = int(st.number_input('DAYS_BIRTH', max_value = 0., step = 1.))
+    DAYS_BIRTH = int(st.number_input('How old is the client ?', max_value = 0., step = 1.))
 
-    DAYS_EMPLOYED = st.number_input('DAYS_EMPLOYED', max_value = 0., step = 1.)
+    DAYS_EMPLOYED = st.number_input('How many days before the application the person started current employment ?',
+                                   max_value = 0., step = 1.)
 
-    DAYS_EMPLOYED_PERC = st.number_input('DAYS_EMPLOYED_PERC', min_value = 0., max_value = 1.)
+    DAYS_EMPLOYED_PERC = st.number_input('What percentage of his lifetime does the time working at the current job represents ?',
+                                        min_value = 0., max_value = 1.)
 
-    DAYS_REGISTRATION = st.number_input('DAYS_REGISTRATION', max_value = 0., step = 1.)
+    DAYS_REGISTRATION = st.number_input('How many days before the application did client change his registration ?',
+                                       max_value = 0., step = 1.)
     
-    DAYS_ID_PUBLISH = int(st.number_input('DAYS_ID_PUBLISH', max_value = 0., step = 1.))
+    DAYS_ID_PUBLISH = int(st.number_input(f'How many days before the application did client change the identity'
+                                          f'document with which he applied for the loan', 
+                                          max_value = 0., step = 1.))
     
-    AMT_ANNUITY = st.number_input('AMT_ANNUITY', min_value = 0., step = 1.)
+    AMT_ANNUITY = st.number_input('Loan annuity', min_value = 0., step = 1.)
     
-    ANNUITY_INCOME_PERC = st.number_input('ANNUITY_INCOME_PERC', min_value = 0., max_value = 1.)
+    ANNUITY_INCOME_PERC = st.number_input('What percentage of the monthly salary does the annuity represents ?',
+                                         min_value = 0., max_value = 1.)
     
-    INSTAL_DBD_MEAN = st.number_input('INSTAL_DBD_MEAN', min_value = 0., step = 1.)
+    INSTAL_DBD_MEAN = st.number_input(f'How many days before the due date for each instalment on average does'
+                                     f'the client usually pay it ?',
+                                     min_value = 0., step = 1.)
     
-    REGION_POPULATION_RELATIVE = st.number_input('REGION_POPULATION_RELATIVE', min_value = 0., max_value = 1.)
+    REGION_POPULATION_RELATIVE = st.number_input('How populated is the region where the client lives ?',
+                                                min_value = 0., max_value = 1.)
     
 
     predict_btn = st.button('Prediction')
   
     if predict_btn:
-          
+
         dictio_pred = {'EXT_SOURCE_1': EXT_SOURCE_1, 'EXT_SOURCE_2': EXT_SOURCE_2, 'EXT_SOURCE_3': EXT_SOURCE_3, 
                        'PAYMENT_RATE': PAYMENT_RATE, 'DAYS_BIRTH': DAYS_BIRTH, 'DAYS_EMPLOYED': DAYS_EMPLOYED,
                        'DAYS_EMPLOYED_PERC': DAYS_EMPLOYED_PERC, 'DAYS_REGISTRATION': DAYS_REGISTRATION, 
@@ -59,12 +68,16 @@ def main():
         st.text("")
         
         if float(response.content) > 0.20:
-            st.write("You can not grant a loan to this client because their default risk is above 20%")
-        else: 
-            st.write("You can grant a loan to this client because their default risk is below 20%")
-        
-        
-        
+            
+            text = "You can not grant a loan to this client because their default risk is above 20%"
+            st.markdown(f'<div style="background-color:#4CAF50;padding:10px;border-radius:5px;'
+                        f'color:white;">{text}</div>',unsafe_allow_html=True)
+        else:
+            
+            text = "You can not grant a loan to this client because their default risk is above 20%"
+            st.markdown(f'<div style="background-color:#FF6347;padding:10px;border-radius:5px;'
+                        f'color:white;">{text}</div>',unsafe_allow_html=True)
+
         PERS_FEAT_API_URL = "https://projet7-api-0c8f5c7ce811.herokuapp.com/prediction/"
         response1 = requests.get(PERS_FEAT_API_URL, params = dictio_pred)
         exp1 = pickle.loads(response1.content)
